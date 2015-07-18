e014ecd023637948b0ed1b982e13fbce1e5716ed
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-07-17 18:09:50 -0600

    Initial Commit

---
+++ PhotoTransfer/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PhotoTransferPanel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PhotoTransferPanel))
+        Me.MainProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.ProgressLabel = New System.Windows.Forms.Label()
+        Me.BeginButton = New System.Windows.Forms.Button()
+        Me.CopyPhoto = New System.Windows.Forms.PictureBox()
+        CType(Me.CopyPhoto, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MainProgressBar
+        '
+        Me.MainProgressBar.Location = New System.Drawing.Point(12, 335)
+        Me.MainProgressBar.Name = "MainProgressBar"
+        Me.MainProgressBar.Size = New System.Drawing.Size(568, 23)
+        Me.MainProgressBar.TabIndex = 0
+        Me.MainProgressBar.UseWaitCursor = True
+        '
+        'ProgressLabel
+        '
+        Me.ProgressLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ProgressLabel.Location = New System.Drawing.Point(12, 313)
+        Me.ProgressLabel.Name = "ProgressLabel"
+        Me.ProgressLabel.Size = New System.Drawing.Size(568, 19)
+        Me.ProgressLabel.TabIndex = 1
+        Me.ProgressLabel.Text = "Waiting to begin..."
+        '
+        'BeginButton
+        '
+        Me.BeginButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BeginButton.Location = New System.Drawing.Point(12, 279)
+        Me.BeginButton.Name = "BeginButton"
+        Me.BeginButton.Size = New System.Drawing.Size(568, 31)
+        Me.BeginButton.TabIndex = 2
+        Me.BeginButton.Text = "Begin Photo Transfer"
+        Me.BeginButton.UseVisualStyleBackColor = True
+        '
+        'CopyPhoto
+        '
+        Me.CopyPhoto.BackColor = System.Drawing.Color.White
+        Me.CopyPhoto.BackgroundImage = CType(resources.GetObject("CopyPhoto.BackgroundImage"), System.Drawing.Image)
+        Me.CopyPhoto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.CopyPhoto.Location = New System.Drawing.Point(12, 12)
+        Me.CopyPhoto.Name = "CopyPhoto"
+        Me.CopyPhoto.Size = New System.Drawing.Size(568, 261)
+        Me.CopyPhoto.TabIndex = 3
+        Me.CopyPhoto.TabStop = False
+        '
+        'PhotoTransferPanel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 370)
+        Me.Controls.Add(Me.CopyPhoto)
+        Me.Controls.Add(Me.BeginButton)
+        Me.Controls.Add(Me.ProgressLabel)
+        Me.Controls.Add(Me.MainProgressBar)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "PhotoTransferPanel"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Photo Transfer"
+        CType(Me.CopyPhoto, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents MainProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents ProgressLabel As System.Windows.Forms.Label
+    Friend WithEvents BeginButton As System.Windows.Forms.Button
+    Friend WithEvents CopyPhoto As System.Windows.Forms.PictureBox
+
+End Class
---
+++ PhotoTransfer/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class PhotoTransferPanel
+    Dim AppStep = 0
+    Dim NumSteps = 7
+    Dim ProgressMax = 700
+    Dim ConfigLoation = "C:\PhotoTransferConfig.ini"
+
+    Private Sub OnFormLoad(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MainProgressBar.Maximum = ProgressMax
+    End Sub
+
+    Private Sub BeginButton_Click(sender As Object, e As EventArgs) Handles BeginButton.Click
+        ' Seven part process
+        ' 1. Cam on
+        ' 2. Cam copy to tmp
+        ' 3. Cam off / eject / manual delete warning
+        ' 4. Organize tmp to c:
+        ' 5. Plug in HDD
+        ' 6. Copy c: to HDD
+        ' 7. Eject HDD, done
+        FindConfig() ' Double check values
+        PlugInCamera() ' Part 1
+        CheckCameraReady() ' Check Part One
+    End Sub
+
+    Private Sub PlugInCamera()
+        Log("Step 1 of 7: Waiting on camera...", True)
+        Dim PICTitle = "Camera Setup"
+        Dim PICMsg = "Please plug in your camera and turn it on. (Click ok when done.)"
+        Dim PICStyle = MsgBoxStyle.OkOnly Or MsgBoxStyle.Information
+        Dim NextStep = MessageBox(PICMsg, PICStyle, PICTitle, False)
+
+        If NextStep = MsgBoxResult.Ok Then
+            AppStep = 1
+            MainProgressBar.Value = (ProgressMax / NumSteps) * AppStep
+            'Return to main program
+        End If
+    End Sub
+
+    Private Sub CheckCameraReady()
+        Log("Step 1 of 7: Checking Camera Availability...", True)
+
+    End Sub
+
+    Private Sub FindConfig()
+        Log("Finding Configuration File At: " + ConfigLoation + " ...", True)
+
+        If My.Computer.FileSystem.FileExists(ConfigLoation) Then
+            Log("Loading Configuration File From: " + ConfigLoation + " ...", True)
+        Else
+            Log("Missing Configuration File At: " + ConfigLoation + " | Creating file...", True)
+            FirstConfigSetup()
+        End If
+    End Sub
+
+    Private Sub FirstConfigSetup()
+        Dim Config As New IniFile
+        Config.AddSection("DRIVE").AddKey("CAMERA_DRIVE_LETTER").Value = "F:"
+        Config.AddSection("DRIVE").AddKey("HDD_DRIVE_LETTER").Value = "G:"
+        Config.AddSection("DRIVE").AddKey("LOCAL_DRIVE_LETTER").Value = "C:"
+
+        Config.AddSection("DRIVE_FOLDER").AddKey("CAMERA").Value = "{CAMERA_DRIVE_LETTER}\DCIM\100ND70S\"
+        Config.AddSection("DRIVE_FOLDER").AddKey("HDD").Value = "{HDD_DRIVE_LETTER}\Pictures\{YEAR_FORMAT}\{MONTH_FORMAT}"
+        Config.AddSection("DRIVE_FOLDER").AddKey("TEMP").Value = "{LOCAL_DRIVE_LETTER}\Users\Jim and Joyce\Pictures\Joyce's Pictures\TMP"
+        Config.AddSection("DRIVE_FOLDER").AddKey("LOCAL").Value = "{LOCAL_DRIVE_LETTER}\Users\Jim and Joyce\Pictures\Joyce's Pictures\{YEAR_FORMAT}\{MONTH_FORMAT}"
+
+        Config.AddSection("RAW_PHOTO_PROPS").AddKey("NAME").Value = "DSC_{NUMBER_SCHEME}"
+        Config.AddSection("RAW_PHOTO_PROPS").AddKey("NUMBER_SCHEME").Value = "####"
+        Config.AddSection("RAW_PHOTO_PROPS").AddKey("STARTIBG_NUMBER").Value = "0001"
+        Config.AddSection("RAW_PHOTO_PROPS").AddKey("FILE_EXT").Value = ".JPG"
+
+        Config.AddSection("FORMAT_STRINGS").AddKey("YEAR_FORMAT").Value = "yyyy"
+        Config.AddSection("FORMAT_STRINGS").AddKey("MONTH_FORMAT").Value = "M - MMMM"
+    End Sub
+
+    Private Function MessageBox(ByRef Msg As String, ByRef Style As MsgBoxStyle, ByRef Title As String, ByRef AppExit As Boolean)
+        Log(Title + ": " + Msg, False)
+        Dim MsgOut = MsgBox(Msg, Style, Title)
+        If AppExit = True Then
+            Application.Exit()
+            End
+        End If
+        Return MsgOut
+    End Function
+
+    Private Sub Log(ByRef Msg As String, ByRef ChangeText As Boolean)
+        Console.WriteLine(Msg)
+        If ChangeText = True Then
+            ProgressLabel.Text = Msg
+        End If
+    End Sub
+End Class
